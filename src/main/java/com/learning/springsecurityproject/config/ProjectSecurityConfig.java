803424996b6f83b35b5542051a6f33441b5cc54b
vtammine <vtammine@ford.com>
2022-11-14 20:40:04 -0500

    Leveraging JdbcDetailsManager for spring security

--- src/main/java/com/learning/springsecurityproject/config/ProjectSecurityConfig.java
+++ src/main/java/com/learning/springsecurityproject/config/ProjectSecurityConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -43,7 +47,7 @@ public class ProjectSecurityConfig {
     }  */
 
 //  Approach 2
-    @Bean
+/*    @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails admin = User.withUsername("admin")
                 .password("12345")
@@ -54,6 +58,11 @@ public class ProjectSecurityConfig {
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(admin,user);
+    } */
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
