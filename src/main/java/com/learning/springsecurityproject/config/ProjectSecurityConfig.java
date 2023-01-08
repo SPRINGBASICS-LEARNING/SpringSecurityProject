b949c0df312a747e36f9eb962735e2ead832b36b
vtammine <vtammine@ford.com>
2023-01-07 20:44:53 -0500

    Added h2 configuration and BcryptPasswordEncoder

--- src/main/java/com/learning/springsecurityproject/config/ProjectSecurityConfig.java
+++ src/main/java/com/learning/springsecurityproject/config/ProjectSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -27,6 +28,8 @@ public class ProjectSecurityConfig {
                                     .and().httpBasic();
 //        http.formLogin();
 //        http.httpBasic();
+//     Below line is for h2-console
+        http.headers().frameOptions().disable();
         return http.build();
     }
 
@@ -67,6 +70,6 @@ public class ProjectSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
