92882f7b3377ee66cd5674a2d9f3f499c35b5857
vtammine <vtammine@ford.com>
2023-02-05 13:19:47 -0500

    Enabled the PreAuthorize annotation

--- src/main/java/com/learning/springsecurityproject/SpringSecurityProjectApplication.java
+++ src/main/java/com/learning/springsecurityproject/SpringSecurityProjectApplication.java
@@ -2,8 +2,10 @@ package com.learning.springsecurityproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SpringSecurityProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/learning/springsecurityproject/repository/LoansRepository.java
+++ src/main/java/com/learning/springsecurityproject/repository/LoansRepository.java
@@ -2,11 +2,14 @@ package com.learning.springsecurityproject.repository;
 
 import com.learning.springsecurityproject.model.Loans;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface LoansRepository extends CrudRepository<Loans, Long> {
+
+    @PreAuthorize("hasRole('ROOT')")
     List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 }
